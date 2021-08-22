9e9ec0bb0099419ad8a92077cd588a24752d6259
Jessica Stern <jlstern3@gmail.com>
2021-08-21 21:08:23 -0400

    Moved Create New Product btn to bottom of list

--- client/src/components/AllProducts.js
+++ client/src/components/AllProducts.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Link} from '@reach/router';
-import {Button} from '@material-ui/core';
+import { Link } from '@reach/router';
+import { Button } from '@material-ui/core';
 
 const AllProducts = (props) => {
-    const[products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/products')
@@ -15,22 +15,23 @@ const AllProducts = (props) => {
             .catch((err) => console.log(err));
     }, []);
 
-    return(
+    return (
         <div>
             <h3>All Products</h3>
-            {/* Button to create new product */}
-            <Link to = {'/api/products/new'}><Button type = "submit" variant = "contained" color="primary">Create New Product</Button></Link>
             {
                 products.map((product, index) => {
                     console.log("this is a new product: " + product.title);
-                    return(
-                        <div key = {index}>
-                            <Link to= {'/api/products/' + product._id}>{product.title}</Link>
+                    return (
+                        <div key={index}>
+                            <Link to={'/api/products/' + product._id}>{product.title}</Link>
                         </div>
                     )
                 })
             }
-        </div>  
+            {/* Button to create new product */}
+            <Link to={'/api/products/new'}><Button type="submit" variant="contained" color="primary">Create New Product</Button></Link>
+
+        </div>
     )
 }
 
