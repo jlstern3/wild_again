a08151d563641cf8fcff7b96be4a6f40fed6a89e
Jessica Stern <jlstern3@gmail.com>
2021-08-22 21:49:57 -0400

    Can delete from PrdDetails pg, routes to AllPrds

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {Link, navigate} from '@reach/router';
 import {Button} from '@material-ui/core';
+import DeleteProduct from './DeleteProduct';
 
 const ProductDetails = (props) => {
     const {id} = props;
@@ -15,7 +16,9 @@ const ProductDetails = (props) => {
             .catch((err) => { console.log(err)});
     },[id]);
 
-
+const afterDeleteHandler = () => {
+    navigate('/api/products');
+}
 
     return(
         <div>
@@ -24,6 +27,9 @@ const ProductDetails = (props) => {
             <h5>Description: {product.description}</h5>
             <Link to = {'/api/products'}><Button variant="contained" color="primary">Back to All Products</Button></Link>
             <Link to = {'/api/products/' + props.id + '/edit'}><Button variant="contained" color="default">Edit {product.title}</Button></Link>
+            <DeleteProduct 
+                id = {props.id}
+                afterDeleteHandler = {afterDeleteHandler}/>
         </div>
     )
 }
