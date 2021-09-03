11ffee4d17c3a32293f4b1b89fea9d26563bfa81
Jessica Stern <jlstern3@gmail.com>
2021-09-02 21:12:53 -0400

    Added links to HomePg for rest of app nav

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from 'react'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {Link} from '@reach/router';
 import TabPanel from './TabPanel';
+import {Button} from '@material-ui/core';
+import { Link, navigate } from '@reach/router';
 
-const HomePage = (props) => {
+const HomePage = () => {
     return(
         <div>
             <h1>This is the home page displaying</h1>
+            <Button type="contained"><Link to ="/api/products">All Your Products</Link></Button>
+            <Button type="contained"><Link to ="/api/products/local">Shop Local</Link></Button>
+            <Button type="contained"><Link to ="/api/products/tips">Helpful Tips</Link></Button>
+
         </div>
     )
 }
