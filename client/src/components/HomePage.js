cd0da4562942bc1d5467143ea4196dc67c54091c
Jessica Stern <jlstern3@gmail.com>
2021-09-03 09:16:39 -0700

    Placeholder blurbs as wireframe content

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -8,6 +8,8 @@ const HomePage = () => {
     return(
         <div>
             <h1>This is the home page displaying</h1>
+            <h3>Placeholder blurb for app -- why it's important to shop locally, reduce waste, etc.</h3>
+            <h5>With all this being said, click on a link below to get started on your sustainable living journey!</h5>
             <Button type="contained"><Link to ="/api/products">All Your Products</Link></Button>
             <Button type="contained"><Link to ="/api/products/local">Shop Local</Link></Button>
             <Button type="contained"><Link to ="/api/products/tips">Helpful Tips</Link></Button>
