24ea8c7f6593fbcc12b7a34db96360d4e2c4f0bf
Jessica Stern <jlstern3@gmail.com>
2021-09-04 08:06:46 -0700

    Navbar present throughout entire app

--- client/src/App.js
+++ client/src/App.js
@@ -12,16 +12,18 @@ import ImageList from './components/ImageList';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faChevronCircleLeft, faPen, faTrashAlt, faLightbulb, faEdit, faPlus } from '@fortawesome/free-solid-svg-icons'
-import Contact from './components/HomePage';
 import {Button} from '@material-ui/core';
+import Navbar from './components/Navbar';
 
 library.add(fab, faChevronCircleLeft, faTrashAlt, faPen, faLightbulb, faEdit, faPlus )
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Wild Again</h1>
       <h3 id="subtitle">Your Sustainable Guide to Living</h3>
+
       <Router>
         <HomePage default path="/api/home" default />
         <CreateProduct path = "/api/products/new" />
