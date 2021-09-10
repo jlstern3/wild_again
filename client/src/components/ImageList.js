b418e0f76365103395805cc0a5982651181271ca
Jessica Stern <jlstern3@gmail.com>
2021-09-09 21:35:30 -0700

    Local AA Resoure ImgList filled out

--- client/src/components/ImageList.js
+++ client/src/components/ImageList.js
@@ -6,12 +6,16 @@ import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-import image from '../img/AlderleafLogo.jpg';
-import umich from '../img/Umich.jpg';
+import whitelotus from '../img/WhiteLotus.jpg';
+import bythepound from '../img/ByThePound.jpg';
 import rtr from '../img/RaiseTheRoot.jpg';
 import pfc from '../img/Co-op.jpg';
+import beejoyful from '../img/BeeJoyful.jpg';
 import argus from '../img/Argus.jpg';
+import homeandgarden from '../img/Downtown-Home-Garden.jpg';
 import byoc from '../img/BYOC.jpg';
+import aathrift from '../img/AAThrift.jpg';
+import mailshoppe from '../img/MailShoppe.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, navigate } from '@reach/router';
 
@@ -34,14 +38,19 @@ const useStyles = makeStyles((theme) => ({
 
 const itemData = [
     {
-        img: byoc,
-        title: 'BYOC Co.',
-        website: 'https://www.byocco.com/',
+        img: whitelotus,
+        title: "White Lotus Farms",
+        website: "https://whitelotusfarms.com/",
     },
     {
-        img: umich,
-        title: "Umich logo",
-        website: "Someone who's now rich",
+        img: beejoyful,
+        title: "Bee Joyful Shop",
+        website: "https://beejoyfulshop.com/",
+    }, 
+    {
+        img: argus,
+        title: "Argus Farm Stop",
+        website: "https://www.argusfarmstop.com/",
     },
     {
         img: rtr,
@@ -49,17 +58,35 @@ const itemData = [
         website: "https://www.raisetherootonline.com/",
     },
     {
-        img: argus,
-        title: "Argus Farm Stop",
-        website: "https://www.argusfarmstop.com/",
+        img: bythepound,
+        title: "By The Pound",
+        website: "https://www.facebook.com/bythelb/",
+    },
+    {
+        img: byoc,
+        title: 'BYOC Co.',
+        website: 'https://www.byocco.com/',
+    },
+    {
+        img: homeandgarden,
+        title: "Downtown Home and Garden",
+        website: "http://www.downtownhomeandgarden.com/",
     },
     {
         img: pfc,
         title: "People's Food Co-op",
         website: "https://peoplesfood.coop/newsite/",
     },
-
-
+    {
+        img: aathrift,
+        title: "Ann Arbor PTO Thrift Shop",
+        website: "https://www.a2ptothriftshop.org/",
+    },
+    {
+        img: mailshoppe,
+        title: "The Mail Shoppe",
+        website: "https://mailshoppe.com/",
+    },
 ]
 
 export default function TitlebarImageList() {
@@ -71,7 +98,7 @@ export default function TitlebarImageList() {
                 <FontAwesomeIcon icon="chevron-circle-left" class="back-icon"></FontAwesomeIcon></Link>            
             <ImageList rowHeight={180} className={classes.imageList}>
                 <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
-                    <ListSubheader component="div">Ann Arbor Resources</ListSubheader>
+                    <ListSubheader component="div" id="ann-arbor">Ann Arbor Resources</ListSubheader>
                 </ImageListItem>
                 {itemData.map((item) => (
                     <ImageListItem key={item.img}>
