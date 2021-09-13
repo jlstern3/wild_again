c18afaad9f969aef3ee9a94d942812173c426bc2
Jessica Stern <jlstern3@gmail.com>
2021-09-13 13:04:55 -0700

    Tooltips added to Edit & Back icon btns

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -4,6 +4,10 @@ import { Link, navigate } from '@reach/router';
 import DeleteProduct from './DeleteProduct';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// Tooltip
+import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles } from '@material-ui/core/styles';
+
 const ProductDetails = (props) => {
     const { id } = props;
     const [product, setProduct] = useState({});
@@ -20,6 +24,21 @@ const ProductDetails = (props) => {
         navigate('/api/products');
     }
 
+
+    const useStyles = makeStyles((theme) => ({
+        absolute: {
+            position: 'absolute',
+            bottom: theme.spacing(2),
+            right: theme.spacing(3),
+        },
+        // format: {
+        //     boxShadow: 'none!important',
+        //     border: 'none!important',
+        // },
+    }));
+
+    const classes = useStyles();
+
     return (
         <div>
             <h3 id="product-details-title">Product Details</h3>
@@ -27,9 +46,11 @@ const ProductDetails = (props) => {
             <h5 class="product-details">Description: <span class="product-details-input">{product.description}</span></h5>
             <h5 class="product-details">Category: <span class="product-details-input">{product.category}</span></h5>
             <h5 class="product-details">Notes: <span class="product-details-input">{product.notes}</span></h5>
+            <Tooltip title='Back' placement='bottom' >
             <Link to={'/api/products'}>
-                <FontAwesomeIcon icon="chevron-circle-left" class="back-icon"></FontAwesomeIcon></Link>
-            <Link to={'/api/products/' + props.id + '/edit'}><FontAwesomeIcon icon="edit" class="edit-icon"></FontAwesomeIcon></Link>
+                <FontAwesomeIcon icon="chevron-circle-left" class="back-icon"></FontAwesomeIcon></Link></Tooltip>
+            <Tooltip title='Edit' placement='bottom'>
+            <Link to={'/api/products/' + props.id + '/edit'}><FontAwesomeIcon icon="edit" class="edit-icon"></FontAwesomeIcon></Link></Tooltip>
             <DeleteProduct
                 id={props.id}
                 afterDeleteHandler={afterDeleteHandler} />
