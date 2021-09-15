3e7512fdf0a57d024006fb054f43bdc989797a0c
Jessica Stern <jlstern3@gmail.com>
2021-09-15 15:30:27 -0400

    Added subtitle & white bg, changed title &spacing

--- client/src/components/AllProducts.js
+++ client/src/components/AllProducts.js
@@ -16,24 +16,22 @@ const AllProducts = (props) => {
             .catch((err) => console.log(err));
     }, []);
 
+
     return (
-        <div>
+        <div class="white-bg">
                 <Link to={'/api/home'}>
                     <FontAwesomeIcon icon="chevron-circle-left" class="back-icon"></FontAwesomeIcon></Link>
-                    <h3 id="all-products-title">Your Created Products</h3>
+                    <h3 id="all-products-title">Your Products</h3>
+                    <p id="all-products-subtitle">Make this space your own!  Keep track of eco-friendly products you want to find, have & love, or want to gift in the future.</p>
                 <Link to={'/api/products/new'}  id="add-new-product-btn">
                     <Button type="submit" type="contained">
                         <FontAwesomeIcon icon="plus"></FontAwesomeIcon>Add a New Product</Button></Link>
-
-
             {
                 products.map((product, index) => {
                     console.log("this is a new product: " + product.title);
                     return (
                         <div key={index}>
                             <ul>
-
-                            
                             <Link to={'/api/products/' + product._id} class="user-made-products"><li class="product">{product.title}</li></Link>
                             </ul>
                         </div>
