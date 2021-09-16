d259beb081db14b3c62b45de82cf097c5dc424aa
Jessica Stern <jlstern3@gmail.com>
2021-09-15 21:34:04 -0400

    Tip categories decided, bullet points added

--- client/src/components/TabPanel.js
+++ client/src/components/TabPanel.js
@@ -68,53 +68,90 @@ export default function ScrollableTabsButtonAuto() {
     return (
         <div className={classes.root}>
             <TabPanel value={value} index={0}>
-                <h3 class="tab-title">Household Items: </h3>
+                <h3 class="tab-title">Kitchen Items: </h3>
                 <ul>
-                    <li>Glass tupperware containers</li>
-                    <li>Washable napkins</li>
-                    <li>Reusable bamboo paper towel</li>
-                    <li>Compostable sponges</li>
+                    <li>Replace plastic resealable containers with glass containers</li>
+                    <li>Replace single-use napkins with washable ones</li>
+                    <li>Replace paper towel with washable towels (like bamboo)</li>
+                    <li>Use reusable, compostable sponges</li>
+                    <li>Replace plastic wrap with beeswax food wrap</li>
+                    <li>Cool items before placing them in the fridge/freezer</li>
+                    <li>Make sure the sink is turned off when not in active</li>
+                    <li>Ensure the fridge/freezer doors are fully shut</li>
+                    <li>Use a stainless steal reusable coffee pod in your single-serving coffee maker</li>
+                    <li>Test your water and get an appropriate water filter</li>
                 </ul>
             </TabPanel>
             <TabPanel value={value} index={1}>
-            <h3 class="tab-title">Cleaning Products: </h3>
+            <h3 class="tab-title">Food: </h3>
                 <ul>
-                    <li>1:1 vinegar:water cleaning spray</li>
-                    <li>Baking soda</li>
-                    <li>Mop that uses towels as mophead</li>
+                    <li>Shop local and seasonal -- reduce those food miles and support local farmers</li>
+                    <li>Make sure your food is free of chemicals and pesticides</li>
+                    <li>Choose fair trade products</li>
+                    <li>Use as much of your food as possible -- there are great recipes online for parts of food that don't typically get discarded like banana peels and watermelon rinds</li>
+                    <li>Get familiar with what you can compost and be diligent about it</li>
+                    <li>Buy in bulk, bring your own containers</li>
+                    <li>Always keep some fair trade bags on hand for transporting your purchases</li>
                 </ul>
             </TabPanel>
             <TabPanel value={value} index={2}>
-            <h3 class="tab-title">Laundry Products: </h3>
+            <h3 class="tab-title">On-the-Go: </h3>
                 <ul>
-                    <li>Placholder</li>
+                    <li>Never leave home without your reusable waterbottle</li>
+                    <li>Bring a reusable mug to your local coffee shop -- you'll often get a discount on your drink</li>
+                    <li>Replace plastic utensils with reusable bamboo or stainless steel ones</li>
+                    <li>Replace plastic straws with a stainless steel, dishwasher safe one</li>
+                    <li>Ditch single-use plastic bags for machine-washable, stain and water resistant-lined snack bags</li>
+                    <li>Always have mesh bags and/or cloth bulk bags on hand, you'll never know when they'll come in handy</li>
+                    <li>Opt for a washable hand towel (they make quick-dry ones) instead of a paper napkin</li>
+                    <li>Pack your own food -- takeout creates a lot of wasted packaging and products</li>
                 </ul>
             </TabPanel>
             <TabPanel value={value} index={3}>
-            <h3 class="tab-title">Home System Designs: </h3>
+            <h3 class="tab-title">Laundry Products: </h3>
                 <ul>
-                    <li>Placholder</li>
+                    <li>Replace dryer sheets with natural wool dryer balls</li>
+                    <li>Only run full loads of laundry</li>
+                    <li>Hang dry when possible</li>
+                    <li>Wear your clothes and use towels multiple times before washing</li>
+                    <li>They make laundry detergent in strips now: hypoallergenic, eco-friendly, liquidless, compostable packaging</li>
+                    <li>If you can't find the strips. opt for solid or powdered detergents free from parabens, phosphates, formaledhyde, and other toxic chemicals</li>
+                    <li>Don't overuse your detergent -- double check to see recommended measurements</li>
                 </ul>
                 
             </TabPanel>
             <TabPanel value={value} index={4}>
-            <h3 class="tab-title">Personal Hygiene: </h3>
+            <h3 class="tab-title">Cleaning Products: </h3>
                 <ul>
-                    <li>Placholder</li>
+                    <li>Stock up on the following ingredients: white vinegar, baking soda, borax, hydrogen peroxide, lemon juice, liquid castile soap, corn meal, olive oil, ethically-produced essential oils</li>
+                    <li>Do a quick Google search of the above ingredients for quick DIY cleaners for all parts of your house</li>
+                    <li>No need for single-use wipes or expensive chemicals (or any toxic chemicals, for that matter)</li>
+                    <li>Choose a mop that uses towels as mophead -- throw the towels in the wash and call it a day</li>
+                    <li>Stock your house with bars of soap instead of plastic pump bottles</li>
                 </ul>
                 
             </TabPanel>
             <TabPanel value={value} index={5}>
-            <h3 class="tab-title">Clothing: </h3>
+            <h3 class="tab-title">Personal Care: </h3>
                 <ul>
-                    <li>Buy second hand!</li>
+                    <li>For menstruation, use a menstrual and/or washable period panties</li>
+                    <li>Avoid chemicals commonly found in personal care products: phthalates, sulfates, parabens, and more (it's worth a quick Google!)</li>
+                    <li>Opt for an eco-friendly, pesticide-free bamboo toothbrush and makeup brushes</li>
+                    <li>Reduce packaging by buying in bulk</li>
+                    <li>Choose products lie chapstick and moisturizers in all-natural, compostable pacakging</li>
+                    <li>Ditch single-use razors for an electric one -- but who says you have to shave?</li>
+                    <li>Instead of packaged face masks with harsh ingredients, make your own natural ones</li>
+                    <li>Avoid aromatherapy candles made of paraffin and check what your incense are made of</li>
                 </ul>
                 
             </TabPanel>
             <TabPanel value={value} index={6}>
-            <h3 class="tab-title">Outdoor Gear: </h3>
+            <h3 class="tab-title">Clothing: </h3>
                 <ul>
-                    <li>Placholder</li>
+                    <li>Buy second hand, or "pre-loved"</li>
+                    <li>Always buy sweatshop free, fair trade clothing</li>
+                    <li>Opt for natural fibers</li>
+                    <li>Host a clothing swap with your friends/family</li>
                 </ul>
             
             </TabPanel>
@@ -128,13 +165,13 @@ export default function ScrollableTabsButtonAuto() {
                     scrollButtons="auto"
                     aria-label="scrollable auto tabs example"
                 >
-                    <Tab label="Household Items" {...a11yProps(0)} />
-                    <Tab label="Cleaning Products" {...a11yProps(1)} />
-                    <Tab label="Laundry" {...a11yProps(2)} />
-                    <Tab label="Home System Designs" {...a11yProps(3)} />
-                    <Tab label="Personal Hygiene" {...a11yProps(4)} />
-                    <Tab label="Clothing" {...a11yProps(5)} />
-                    <Tab label="Outdoor Gear" {...a11yProps(6)} />
+                    <Tab label="Kitchen" {...a11yProps(0)} />
+                    <Tab label="Food" {...a11yProps(1)} />
+                    <Tab label="On-the-Go" {...a11yProps(2)} />
+                    <Tab label="Laundry" {...a11yProps(3)} />
+                    <Tab label="Cleaning" {...a11yProps(4)} />
+                    <Tab label="Personal Care" {...a11yProps(5)} />
+                    <Tab label="Clothing" {...a11yProps(6)} />
                 </Tabs>
             </AppBar>
 
