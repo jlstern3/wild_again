b8623096fde30cad71356453814793e755154c6c
Jessica Stern <jlstern3@gmail.com>
2021-12-04 18:39:56 -0500

    Configured cors & cookie parser on server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
+require("dotenv").config();
+
 // import third party libraries
 const express = require('express');
 const app = express();
 const port = 8000;
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-// require('dotenv').config();
+
 
 // configure server
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
+app.use(cookieParser());
 
 
 // connect mongoose
