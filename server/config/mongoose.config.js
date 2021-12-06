add39e1c54d13516c691069358bf7002ae16b831
Jessica Stern <jlstern3@gmail.com>
2021-12-05 20:52:23 -0500

    Db name not hard coded anymore -- variable in .env

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const dbName = "wild_db";
-// const dbName = process.env.DB_NAME;
+const dbName = process.env.DB_NAME;
+
+
 
 // const uri = process.env.ATLAS_URI;
 // mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
@@ -11,7 +12,7 @@ mongoose.connect('mongodb://localhost/' + dbName, {
     useUnifiedTopology: true,
     // useCreateIndex: true,
 })
-    .then(() => console.log(`Established connect to the "${dbName}" database`))
-    .catch(err => console.log(`Error establishing "${dbName}" connection`, err));   
-    // .then(() => console.log("Established connect to the " + process.env.DB_NAME + " database"))
-    // .catch(err => console.log("Error establishing " + process.env.DB_NAME +  "connection", err)); 
+    // .then(() => console.log(`Established connect to the "${dbName}" database`))
+    // .catch(err => console.log(`Error establishing "${dbName}" connection`, err));   
+    .then(() => console.log("Established connect to the " + process.env.DB_NAME + " database"))
+    .catch(err => console.log("Error establishing " + process.env.DB_NAME +  "connection", err)); 
